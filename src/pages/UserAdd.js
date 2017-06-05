d7bec19b9912a147e120f21d4664d2963407fe23
prodigalyijun <1015752610@qq.com>
2017-06-05 15:16:41 +0800

    优化

--- src/pages/UserAdd.js
+++ src/pages/UserAdd.js
@@ -1,85 +1,121 @@
 import React from 'react';
+import formProvider from '../utils/formProvider';
+import HomeLayout from '../layouts/HomeLayout';
 
 class UserAdd extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      name: '',
-      age: 0,
-      gender: ''
-    };
-  }
-
-  handleValueChange(field, value, type = 'string') {
-    if (type === 'number') {
-      value += value;
-    }
-
-    this.setState({
-      [field]: value
-    });
-  }
-
   handleSubmit(e) {
     e.preventDefault();
 
-    const { name, age, gender } = this.state;
-    fetch('http://localhost:3000/user',{
-      method:'post',
-      body:JSON.stringify({
-        name,
-        age,
-        gender
+    const { form: { name, age, gender }, formValid } = this.props;
+    if (!formValid) {
+      alert('请填写正确的信息后重试');
+      return;
+    }
+
+    fetch('http://localhost:3000/user', {
+      method: 'post',
+      body: JSON.stringify({
+        name: name.value,
+        age: age.value,
+        gender: gender.value
       }),
-      headers:{
-        'Content-Type':'application/json'
+      headers: {
+        'Content-Type': 'application/json'
       }
     })
-    .then((res)=>res.json())
-    .then((res)=>{
-      if(res.id){
-        alert('添加用户成功');
-        this.setState({
-          name:'',
-          age:0,
-          gender:''
-        });
-      }else{
-        alert('添加失败');
-      }
-    })
-    .catch((err)=>console.error(err));
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.id) {
+          alert('添加用户成功');
+          this.context.router.push('/user/list');
+        } else {
+          alert('添加失败');
+        }
+      })
+      .catch((err) => console.error(err));
   }
-
   render() {
-    const { name, age, gender } = this.state;
+    const { form: { name, age, gender }, onFormChange } = this.props;
     return (
-      <div>
-        <header>
-          <h1>添加用户</h1>
-        </header>
-
-        <main>
-          <form onSubmit={(e) => this.handleSubmit(e)}>
-            <label>用户名：</label>
-            <input type="text" value={name} onChange={(e) => this.handleValueChange('name', e.target.value)} />
-            <br />
-            <label>年龄：</label>
-            <input type="number" value={age || ''} onChange={(e) => this.handleValueChange('age', e.target.value, 'number')} />
-            <br />
-            <label>性别：</label>
-            <select value={gender} onChange={(e) => this.handleValueChange('gender', e.target.value)}>
-              <option value="">请选择</option>
-              <option value="male">男</option>
-              <option value="female">女</option>
-            </select>
-            <br /><br />
-            <input type="submit" value="提交" />
-          </form>
-        </main>
-      </div>
+      <HomeLayout title="添加用户">
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <label>用户名：</label>
+          <input
+            type="text"
+            value={name.value}
+            onChange={(e) => onFormChange('name', e.target.value)}
+          />
+          {!name.valid && <span>{name.error}</span>}
+          <br />
+          <label>年龄：</label>
+          <input
+            type="number"
+            value={age.value || ''}
+            onChange={(e) => onFormChange('age', +e.target.value)}
+          />
+          {!age.valid && <span>{age.error}</span>}
+          <br />
+          <label>性别：</label>
+          <select
+            value={gender.value}
+            onChange={(e) => onFormChange('gender', e.target.value)}
+          >
+            <option value="">请选择</option>
+            <option value="male">男</option>
+            <option value="female">女</option>
+          </select>
+          {!gender.valid && <span>{gender.error}</span>}
+          <br />
+          <br />
+          <input type="submit" value="提交" />
+        </form>
+      </HomeLayout>
     );
   }
 }
+
+UserAdd.contextTypes = {
+  router: React.PropTypes.object.isRequired
+};
+
+UserAdd = formProvider({
+  name: {
+    defaultValue: '',
+    rules: [
+      {
+        pattern: function (value) {
+          return value.length > 0;
+        },
+        error: '请输入用户名'
+      },
+      {
+        pattern: /^.{1,4}$/,
+        error: '用户名最多4个字符'
+      }
+    ]
+  },
+  age: {
+    defaultValue: 0,
+    rules: [
+      {
+        pattern: function (value) {
+          return value >= 1 && value <= 100;
+        },
+        error: '请输入1~100的年龄'
+      }
+    ]
+  },
+  gender: {
+    defaultValue: '',
+    rules: [
+      {
+        pattern: function (value) {
+          return !!value;
+        },
+        error: '请选择性别'
+      }
+    ]
+  }
+})(UserAdd);
 
 export default UserAdd;
